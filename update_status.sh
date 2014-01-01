a9f7b57b74e80a61a026f7982a9133a2dfe7d53c
Manuel Munz <manu@somakoma.de>
2014-01-01 20:14:26 +0100

    check both AP interfaces and remove useless cut

--- update_status.sh
+++ update_status.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# 
+#
 # Update Script for SpaceAPI - https://github.com/openlab-aux/spaceapi
-# You obviously need to change the token and the WLAN Interface.
+# You obviously need to change the token and the WLAN Interface(s).
 
-count=$(iwinfo wlan0-1 associnfo | grep SNR | cut -d " " -f 1 | wc -l)
+count=$( (iwinfo wlan0-1 assoclist; iwinfo wlan1 assoclist) | grep SNR | wc -l)
 echo $count
-wget "http://api.openlab-augsburg.de/spacecgi.py?token=lolnope&update_device_count=$count" -O /dev/null
+wget -q "http://api.openlab-augsburg.de/spacecgi.py?token=lolnope&update_device_count=$count" -O /dev/null
